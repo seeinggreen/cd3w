e2399f5bc97e251aea5a85b0c6f93bdfb6173a7d
Dan Gren <dan.green@hw.ac.uk>
2023-02-10 20:03:16 +0000

    First functions added

--- main.py
+++ main.py
@@ -5,32 +5,48 @@ import os;
 local_build_path = "../ai2thor/unity/builds/thor-Linux64-local/thor-Linux64-local";
 image_dir = "/Images/";
 
+def name_to_object_id(name, controller):
+    objs = controller.last_event.metadata['objects'];
+    named_objs = [o for o in objs if o['name'] == name];
+    if len(named_objs) == 1:
+        return named_objs[0]['objectId'];
+    else:
+        return None;
+
+def pickup(name,controller):
+    object_id = name_to_object_id(name, controller.last_event);
+    controller.step(action="PickupObject",objectId=object_id);
+    controller.step(action="Done");
+
 if __name__ == "__main__":
     controller = Controller(height=1200,width=1600,local_executable_path=local_build_path,
                             fieldOfView=120,snapToGrid=False,image_dir=image_dir);
     controller.step(action="Teleport",position=dict(x=0.25,y=1,z=0),rotation=dict(x=0,y=270,z=0),horizon=70,forceAction=True)
 
+    #Place blue square
     controller.step(action="PlaceObjectAtPoint",objectId="Undefined|-00.47|+01.14|+05.42",position={'x': -0.3, 'y': 1.3, 'z': 0.2},rotation={'x':0,'y':0,'z':0})
     controller.step(action="PlaceObjectAtPoint",objectId="Undefined|-00.47|+01.14|+05.42",position={'x': -0.3, 'y': 1.3, 'z': 0.2},rotation={'x':0,'y':0,'z':0})
-
+    #Place red circle
     controller.step(action="PlaceObjectAtPoint",objectId="Undefined|-00.47|+01.14|+05.91",position={'x': 0, 'y': 1.3, 'z': 0.5},rotation={'x':0,'y':0,'z':0})
     controller.step(action="PlaceObjectAtPoint",objectId="Undefined|-00.47|+01.14|+05.91",position={'x': 0, 'y': 1.3, 'z': 0.5},rotation={'x':0,'y':0,'z':0})
-    
+    #Place green apple
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.99",position={'x': 0.1, 'y': 1.3, 'z': -0.3},rotation={'x':0,'y':0,'z':0})
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.99",position={'x': 0.1, 'y': 1.3, 'z': -0.3},rotation={'x':0,'y':0,'z':0})
-    
+    #Place red apple
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.52",position={'x': 0.3, 'y': 1.3, 'z': 0.6},rotation={'x':0,'y':0,'z':0})
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.52",position={'x': 0.3, 'y': 1.3, 'z': 0.6},rotation={'x':0,'y':0,'z':0})
 
-    event = controller.step(action="Done");
-    cv2.imwrite(os.path.join(image_dir,"start.png"),event.cv2img);
+    #Update display and save start image
+    controller.step(action="Done");
+    cv2.imwrite(os.path.join(image_dir,"start.png"),controller.last_event.cv2img);
     
-    
+    #Move green apple
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.99",position={'x': -0.3, 'y': 1.3, 'z': 0.2},rotation={'x':0,'y':0,'z':0})
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.99",position={'x': -0.3, 'y': 1.3, 'z': 0.2},rotation={'x':0,'y':0,'z':0})
-    
+    #Move red apple
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.52",position={'x': 0, 'y': 1.3, 'z': 0.5},rotation={'x':0,'y':0,'z':0})
     controller.step(action="PlaceObjectAtPoint",objectId="Apple|-00.47|+01.14|+04.52",position={'x': 0, 'y': 1.3, 'z': 0.5},rotation={'x':0,'y':0,'z':0})
-    event = controller.step(action="Done")
     
-    cv2.imwrite(os.path.join(image_dir,"end.png"),event.cv2img);
+    #Update display and save end image
+    controller.step(action="Done")
+    cv2.imwrite(os.path.join(image_dir,"end.png"),controller.last_event.cv2img);
