6e2951328b390cec361e5815ca75a2511f0aaa68
Sabrina <sabrina.mccallum@outlook.com>
2023-03-17 20:22:04 +0000

    Exclude src/slurk from gitignore

---
+++ src/slurk/bots/leaderbot/leader_bot.py
@@ -0,0 +1,94 @@
+import requests
+import socketio
+
+
+class LeaderBot:
+    def __init__(self, token, user, host, port, task, rasa_service, level, variant):
+        self.token = token
+        self.user = user
+        self.host = host
+        self.port = port
+
+        self.rasa_service = rasa_service
+
+        self.level = level
+        self.variant = variant
+
+        self.follower = None
+        self.round_in_progress = False
+
+        self.sio = socketio.Client(logger=True)
+
+        self.task_id = task
+        self.sio.on("new_task_room", self.join_task_room())
+
+        self.register_join_get_scene_handler()
+        self.register_message_handler()
+
+    def join_task_room(self):
+        """Let the bot join an assigned task room."""
+
+        def join(data):
+            if self.task_id is None or data["task"] != self.task_id:
+                return
+
+            response = requests.post(
+                f"{self.host}:{self.port}/slurk/api/users/{self.user}/rooms/{data['room']}",
+                headers={"Authorization": f"Bearer {self.token}"},
+            )
+
+        return join
+
+    def register_join_get_scene_handler(self):
+        @self.sio.event
+        def status(data):
+            if data["type"] == "join":
+                self.rasa_service.get_scene(level=self.level, variant=self.variant)
+
+    def register_command_handler(self):
+        @self.sio.event
+        def command(data):
+            if data["user"]["id"] == self.user:
+                return
+
+            if data["command"].lower() == "ready":
+                if "follower" in data["user"]["name"].lower():
+                    self.follower = data["user"]
+                if self.follower:
+                    if self.round_in_progress:
+                        return
+
+                    self.round_in_progress = True
+            elif (
+                data["command"].lower() == "done"
+                and data["user"]["id"] == self.follower["id"]
+            ):
+                self.rasa_service.reset()
+                self.follower = None
+                self.round_in_progress = False
+
+    def register_message_handler(self):
+        @self.sio.event
+        def message(data):
+            if data["user"]["id"] == self.user:
+                return
+
+            leader_message = self.rasa_service.get_response(data["message"])
+
+            self.sio.emit(
+                "text",
+                {
+                    "room": data["room"],
+                    "message": leader_message,
+                },
+            )
+
+    def run(self):
+        self.sio.connect(
+            f"{self.host}:{self.port}",
+            headers={"Authorization": f"Bearer {self.token}", "user": str(self.user)},
+            namespaces="/",
+        )
+
+        # wait until the connection with the server ends
+        self.sio.wait()
