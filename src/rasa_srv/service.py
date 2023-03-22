10be67a5d3fbeac0cbe7158b7fc41d6210040faa
xecarlox94 <jf94.uk@gmail.com>
2023-03-22 15:08:18 +0000

    finished getting rcpts

--- src/rasa_srv/service.py
+++ src/rasa_srv/service.py
@@ -2,6 +2,9 @@ import json
 from functools import reduce
 from ithor.utils.items import Items
 
+from functools import reduce
+
+import requests
 
 
 class RasaException(Exception):
@@ -24,12 +27,83 @@ get_att_OrDict = lambda d, k: d[k] if k in d else d
 
 
 
+
+def get_items_matrix(m, list_items, get_item_fields):
+
+    get_item = lambda l, name: list(filter(lambda item: item['name'] ==name, l))[0]
+
+    items_flat_lst = list(
+        map(
+            lambda x: get_item_fields(x),
+            filter(
+                lambda x: x[2] != None,
+                reduce(
+                    lambda x, y: x+y,
+                    map(
+                        lambda i_lst: list(map(
+                            lambda i_e: (
+                                i_lst[0],
+                                i_e[0],
+                                None if i_e[1] == None else get_item(list_items, i_e[1])
+                            ),
+                            enumerate(i_lst[1])
+                        )),
+                        enumerate(m)
+                    )
+                )
+            )
+        )
+    )
+
+    return items_flat_lst
+
+
+def get_rcpt_data(tupl):
+
+    x=tupl[0]
+    y=tupl[1]
+
+    rcpt_dta = tupl[2]
+
+    shape = "circle" if "ircle" in rcpt_dta['name'] else "square"
+
+    return {
+        "name": rcpt_dta['name'],
+        "shape": shape,
+        "colour": rcpt_dta["colour"].lower(),
+        "pos": (x, y)
+    }
+
+def get_rcpts(m, list_rcpts):
+    return get_items_matrix(m, list_rcpts, get_rcpt_data)
+
+
+def get_element_matrix(m, name_element, name_attribute=False):
+
+    f = lambda d:d
+
+    if name_attribute != False: 
+        f = lambda d: get_att_OrDict(d, name_attribute)
+
+    els = list(filter(
+        lambda e: e != None and e['name'] == name_element,
+        reduce(
+            lambda xs, x: xs+x, 
+            m
+        )
+    ))
+
+    if len(els) == 0: return False
+
+    return f(els[0])
+
+
 class RasaService:
-    def __init__(self,port):
+    def __init__(self,port, sender_id=0):
         self.scene = None
         self.slurk_port = port
         self.metadata_objects, self.metadata_mats = self._get_metadata()
-
+        self.sender_id = sender_id
 
     def _get_metadata(self):
         assets = Items().assets
@@ -58,34 +132,35 @@ class RasaService:
         print("getting scene")
         print("\n\nscene: ", self.scene['mats'])
 
-        self.scene['mats'] = self.map_matrix(
+        self.scene['mats'] = get_rcpts(
             self.scene['mats'], 
-            self.metadata_mats,
-            lambda mat: {
-                'name': mat['name'],
-                'colour': mat['colour'],
-                'is_mat': mat['is_mat'],
-                'home_pos': mat['home_pos']
-            }
+            self.metadata_mats
         )
 
-        print("\n\n NEW scene: ", self.scene['mats'])
+        print("\n\nNEW scene: ", self.scene['mats'])
 
-        print("\n\nget element: ", self.get_element_matrix(self.scene['mats'], "Circle4"))
+        print("\n\nobjs: ", self.scene['mats'])
+
+
+        #print("elements: ", self.get_element_matrix(
+            #self.scene['mats'],
+            #"Square2"
+        #))
+
 
         return self.scene['mats']
 
         # this is the method called when the bot joins the Slurk room
         # it makes the scene config of the current game available to be used in Rasa
 
-    def get_response(self, follower_message, sender_id):
+    def get_response(self, follower_message):
 
-        res = send_msg_rasa(sender_id, follower_message)
+        res = send_msg_rasa(self.sender_id, follower_message)
 
         if is_res_success(res):
             return get_res_msg(res)
         else:
-            raise RasaException("sender_id: ", sender_id, ", message: ", follower_message)
+            raise RasaException("sender_id: ", self.sender_id, ", message: ", follower_message)
         # this is the method called when the follower types a message in Slurk
         # IMPORTANT regardless of how we handle the passing of the message to Rasa
         # and getting the bot generated response, this method needs to return
@@ -96,42 +171,4 @@ class RasaService:
         self.scene = None
         # and whatever other stuff you need to reset
 
-
-
-    def map_matrix(self, m, list_items, f_item=lambda x: x):
-
-        get_item = lambda l, name: list(filter(lambda item: item['name'] ==name, l))[0]
-
-        return list(map(
-            lambda lst: list(map(
-                lambda l_element_name: (
-                    None if l_element_name == None else f_item(
-                        get_item(list_items, l_element_name)
-                    )
-                ),
-                lst
-            )),
-            m
-        ))
-
-    
-
-    def get_element_matrix(self, m, name_element, name_attribute=False):
-
-        f = lambda d:d
-
-        if name_attribute != False: 
-            f = lambda d: get_att_OrDict(d, name_attribute)
-
-        els = list(filter(
-            lambda e: e != None and e['name'] == name_element,
-            reduce(
-                lambda xs, x: xs+x, 
-                m
-            )
-        ))
-
-        if len(els) == 0: return False
-
-        return f(els[0])
 
