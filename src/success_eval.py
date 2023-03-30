93ee4c0ee2d64887963346dd296bcf8747f05ef5
Sabrina <sabrina.mccallum@outlook.com>
2023-03-30 19:44:54 +0100

    create eval script and save results and error logs for all evaluation scenes

--- src/success_eval.py
+++ src/success_eval.py
@@ -12,15 +12,16 @@ from rasa_srv.service import BOT_VARIANT_MAPPING
 
 
 class Metric(ABC):
-    def __init__(self, goal_scene, final_scenes, state_change):
+    def __init__(self, goal_scene, final_scenes, state_change, variant):
         self.goal_scene = goal_scene
         self.final_scenes = final_scenes
         self.state_change = state_change
         self.expected_changes = 7 if state_change else 6
-        self.missing_pos_changes = 0
-        self.missing_state_changes = 0
-        self.results = []
+        self.variant = variant
+        self.missing_changes = {}
+        self.results = {}
         self.result_type = ""
+        self.error_logs = {self.variant: {}}
 
     def _get_state(self, object):
         state = re.sub("\w*\d+", "", object)  # Discard the name and number
@@ -32,69 +33,75 @@ class Metric(ABC):
     def _get_type(self, object):
         return re.sub("\d+\w*", "", object)
 
+    def _log_append(self, info, state_change=False):
+        log = ["===" * 10]
+        for item in info:
+            log.append(item)
+        if not info[-1]:
+            log.append("final state has nothing in this position")
+        else:
+            log.append("different name in pos")
+        if state_change:
+            log.append("missing state change")
+        else:
+            log.append("missing pos change")
+        return log
+
     def _count_changes(self):
         goal_objects = {}
         final_objects = {}
 
-        def rearrange(double_nested_list):
-            rearranged_list = []
-            for i in range(len(double_nested_list[0])):
-                rearranged_list.append([l[i] for l in double_nested_list])
-            return rearranged_list
-
-        for final_scene in self.final_scenes.values():
-            scenes_zipped = zip(self.goal_scene, final_scene["objects"])
-            for i, rows in enumerate(scenes_zipped):
-                rows_rearranged = rearrange(rows)
-                for j, objects in enumerate(rows_rearranged):
-                    if objects[0]:
+        for user, final_scene in self.final_scenes.items():
+            if user == "E5":
+                user == "malek"
+            else:
+                user = user.lower()
+            error_log = []
+            missing_pos_changes = 0
+            missing_state_changes = 0
+            final_objects[user] = {}
+            for i, rows in enumerate(self.goal_scene):
+                for j, object in enumerate(rows):
+                    if object:
                         goal_objects[f"{i}{j}"] = {
-                            "name": objects[0],
-                            "type": self._get_type(objects[0]),
-                            "state": self._get_state(objects[0]),
+                            "name": object,
+                            "type": self._get_type(object),
+                            "state": self._get_state(object),
                         }
-                    if objects[1]:
-                        final_objects[f"{i}{j}"] = {
-                            "name": objects[1],
-                            "type": self._get_type(objects[1]),
-                            "state": self._get_state(objects[1]),
+                    if final_scene["objects"][i][j]:
+                        final_objects[user][f"{i}{j}"] = {
+                            "name": final_scene["objects"][i][j],
+                            "type": self._get_type(final_scene["objects"][i][j]),
+                            "state": self._get_state(final_scene["objects"][i][j]),
                         }
 
             for pos, object in goal_objects.items():
-                print(pos)
-                print(object)
-                if pos in final_objects:
-                    print("in final")
-                    if final_objects[pos]["name"] != object["name"]:
-                        print("different name in pos")
+                if pos in final_objects[user]:
+                    info = [
+                        f"{pos[0]}-{pos[1]}",
+                        object["name"],
+                        final_objects[user][pos]["name"],
+                    ]
+                    if final_objects[user][pos]["name"] != object["name"]:
                         if self.state_change:
-                            if final_objects[pos]["type"] != object["type"]:
-                                self.missing_pos_changes += 1
-                                print(final_objects[pos])
-                                print(object)
-                                print(f"missing pos change {self.missing_pos_changes}")
-                                print("==" * 10)
-                            elif final_objects[pos]["state"] != object["state"]:
-                                print(final_objects[pos])
-                                print(object)
-                                self.missing_state_changes += 1
-                                print(
-                                    f"missing state change {self.missing_state_changes}"
+                            if final_objects[user][pos]["type"] != object["type"]:
+                                missing_pos_changes += 1
+                                error_log.append(self._log_append(info))
+                            elif final_objects[user][pos]["state"] != object["state"]:
+                                missing_state_changes += 1
+                                error_log.append(
+                                    self._log_append(info, state_change=True)
                                 )
-                                print("==" * 10)
                         else:
-                            print(final_objects[pos])
-                            print(object)
-                            self.missing_pos_changes += 1
-                            print(f"missing state change {self.missing_state_changes}")
-                            print("==" * 10)
+                            missing_pos_changes += 1
+                            error_log.append(self._log_append(info))
                 else:
-                    print("Final state has nothing in this position")
-                    print(pos)
-                    print(final_objects.keys())
-                    self.missing_pos_changes += 1
-                    print(f"missing pos change {self.missing_pos_changes}")
-                    print("==" * 10)
+                    info = [pos, object, ""]
+                    missing_pos_changes += 1
+                    error_log.append(self._log_append(info))
+
+            self.missing_changes[user] = [missing_pos_changes, missing_state_changes]
+            self.error_logs[self.variant][user] = error_log
 
     @abstractmethod
     def compute_metric(self):
@@ -108,10 +115,11 @@ class Success(Metric):
 
     def compute_metric(self):
         self._count_changes()
-        if self.missing_pos_changes or self.missing_state_changes:
-            self.results.append(float(0))
-        else:
-            self.results.append(float(1))
+        for user, counts in self.missing_changes.items():
+            if counts[0] or counts[1]:
+                self.results[user] = float(0)
+            else:
+                self.results[user] = float(1)
 
 
 class SuccessGC(Metric):
@@ -121,26 +129,23 @@ class SuccessGC(Metric):
 
     def compute_metric(self):
         self._count_changes()
-        correct_changes = self.expected_changes - (
-            self.missing_pos_changes + self.missing_state_changes
-        )
-        changes_made_relative = correct_changes / self.expected_changes
-        self.results.append(changes_made_relative)
+        for user, count in self.missing_changes.items():
+            correct_changes = self.expected_changes - sum(count)
+            changes_made_relative = correct_changes / self.expected_changes
+            self.results[user] = changes_made_relative
 
 
-class Results:
-    def __init__(self, level, variant, result_type, save_level_first):
+class Evaluator:
+    def __init__(self, level, variant, result_type, save_bot_first, eval_date):
         self.level = level
         self.state_change = True if self.level == 6 else False
         self.variant = variant
         self.result_type = result_type
-        self.experiment_date = "2023-03-08"  # change to eval date
+        self.experiment_date = eval_date
         self.goal_scene = self._get_goal_scene()
         self.final_scenes = self._get_final_scenes()
-        self.results = self._get_results()
-        self.save_level_first = save_level_first
-        # self.bot_variant = f"b{BOT_VARIANT_MAPPING["v5"]}"
-        self.bot_variant = f"b{BOT_VARIANT_MAPPING['v5']}"  # change to above line
+        self.save_bot_first = save_bot_first
+        self.bot_variant = f"b{BOT_VARIANT_MAPPING[self.variant]}"
         self.results_dir = f"{os.path.abspath('')}/results"
 
     def _get_goal_scene(self):
@@ -153,59 +158,65 @@ class Results:
 
     def _get_final_scenes(self):
         with open(
-            f"output/train_final_states/{self.level}_{self.variant}.json",
+            f"output/final_states/{self.level}_{self.variant}.json",
             encoding="utf-8",
         ) as json_file:
             final_scenes = json.load(json_file)[self.experiment_date]
 
         return final_scenes
 
-    def _get_results(self):
+    def get_results(self):
         if self.result_type == "success":
-            metric = Success(self.goal_scene, self.final_scenes, self.state_change)
+            metric = Success(
+                self.goal_scene, self.final_scenes, self.state_change, self.variant
+            )
         else:
-            metric = SuccessGC(self.goal_scene, self.final_scenes, self.state_change)
+            metric = SuccessGC(
+                self.goal_scene, self.final_scenes, self.state_change, self.variant
+            )
         metric.compute_metric()
-        return metric.results
+        return metric.results, metric.error_logs
 
-    def save(self):
-        if self.save_level_first:
-            level_1 = self.level
-            level_2 = self.bot_variant
-        else:
+    def save(self, data, logs=False):
+        if self.save_bot_first or logs:
             level_1 = self.bot_variant
             level_2 = self.level
+        else:
+            level_1 = self.level
+            level_2 = self.bot_variant
         if not os.path.exists(self.results_dir):
             os.mkdir(self.results_dir)
-        results_path = f"{self.results_dir}/{self.result_type}.json"
-        if os.path.exists(results_path):
-            with open(results_path, encoding="utf-8") as json_file:
-                existing_results_data = json.load(json_file)
-            if level_1 in existing_results_data:
-                if level_2 in existing_results_data[level_1]:
-                    existing_results_data[level_1][level_2] = self.results
-                else:
-                    existing_results_data[level_1].update({level_2: self.results})
-            else:
-                existing_results_data.update({level_1: {level_2: self.results}})
-            with open(results_path, "w+") as outfile:
-                json.dump(existing_results_data, outfile)
+        if logs:
+            fn = f"{self.results_dir}/{self.result_type}_logs.json"
         else:
-            with open(results_path, "w+") as outfile:
-                json.dump({level_1: {level_2: self.results}}, outfile)
+            fn = f"{self.results_dir}/{self.result_type}.json"
+        existing_data = {}
+        if os.path.exists(fn):
+            with open(fn, encoding="utf-8") as json_file:
+                existing_data = json.load(json_file)
+            if level_1 in existing_data:
+                existing_data[level_1][level_2] = data
+            else:
+                existing_data[level_1] = {level_2: data}
+        else:
+            existing_data[level_1] = {level_2: data}
+        with open(fn, "w+") as outfile:
+            json.dump(existing_data, outfile)
 
 
 if __name__ == "__main__":
-    # for result_type in ["success", "success_gc", "misplaced", "fixed_strict"]:
-    #     for fn in os.listdir(f"{os.path.abspath('')}/output/train_final_states"):
-    #         print(fn)
-    #         level, variant = fn.replace(".json", "").split("_")
-    #         print(level)
-    #         print(variant)
-    #         try:
-    #             results = Results(level, variant, result_type, True)
-    #             results.save()
-    #         except:
-    #             pass
-    results = Results("l7", "v3", "success_gc", True)
-    results.save()
+    for result_type in ["success", "success_gc"]:
+        for fn in os.listdir(f"{os.path.abspath('')}/output/final_states"):
+            level, variant = fn.replace(".json", "").split("_")
+            if level in ["l1", "l4", "l7"] and variant in [
+                f"v{i}" for i in range(5, 11)
+            ]:
+                print(level)
+                print(variant)
+                for bot_first_bool in [True, False]:
+                    evaluator = Evaluator(
+                        level, variant, result_type, bot_first_bool, "2023-03-29"
+                    )
+                    results, error_logs = evaluator.get_results()
+                    evaluator.save(error_logs, logs=True)
+                    evaluator.save(results)
